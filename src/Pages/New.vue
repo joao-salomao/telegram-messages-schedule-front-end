b082eb93e5f9b31fdb117f2bf78cf8aface63e0d
helenatamanini <75807270+helenatamanini@users.noreply.github.com>
2021-03-01 20:56:33 -0300

    criando componente de cadastro

---
+++ src/Pages/New.vue
@@ -0,0 +1,25 @@
+<template>
+  <Modal>
+    <h1>Cadastrando mensagem</h1>
+    <Form @cancel="$router.back()" @save="createMessage($event)" />
+  </Modal>
+</template>
+<script>
+import api from "@/api";
+import Modal from "@/components/Modal";
+import Form from "@/components/Form";
+export default {
+  components: {
+    Modal,
+    Form,
+  },
+  methods: {
+    createMessage(form) {
+      api.createMessage(form).then(() => {
+        this.$router.back();
+        this.$emit("message-created");
+      });
+    },
+  },
+};
+</script>
