a107ccb146e1aefede2bf072a1371f164cbee775
João Salomão <38229718+joao-salomao@users.noreply.github.com>
2021-03-01 21:06:01 -0300

    Criando componente da home

---
+++ src/Pages/Home.vue
@@ -0,0 +1,20 @@
+<template >
+  <div id="background">
+    <h1 id="title">Telegram messages</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+#title {
+  background: rgb(73, 119, 153);
+  padding: 40px;
+  margin: 0px 0px 50px 0px;
+  text-align: center;
+  color: rgb(255, 255, 255);
+  font-weight: bold;
+}
+#background {
+  background: rgb(231, 235, 240);
+}
+</style>
